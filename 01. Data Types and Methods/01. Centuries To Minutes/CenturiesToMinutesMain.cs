244319bc8d04b64f5efc5bb8bd949e2060379937
Housey <housebreaker1@gmail.com>
2016-05-17 14:40:44 +0300

    add first exercise

---
+++ 01. Data Types and Methods/01. Centuries To Minutes/CenturiesToMinutesMain.cs
@@ -0,0 +1,23 @@
+﻿namespace _01.Centuries_To_Minutes
+{
+	using System;
+
+	public static class CenturiesToMinutesMain
+	{
+		public static void Main()
+		{
+			var centuries = int.Parse(Console.ReadLine());
+
+			var years = centuries * 100;
+			var days = Math.Round(years * 365.242);
+			var hours = days * 24;
+			var minutes = hours * 60;
+
+			Console.WriteLine(centuries);
+			Console.WriteLine(years);
+			Console.WriteLine(days);
+			Console.WriteLine(hours);
+			Console.WriteLine(minutes);
+		}
+	}
+}
