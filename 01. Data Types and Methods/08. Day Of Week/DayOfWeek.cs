4220980b257076b588a968567825803fae62b412
Housey <housebreaker1@gmail.com>
2016-05-17 15:48:27 +0300

    add problem 8

---
+++ 01. Data Types and Methods/08. Day Of Week/DayOfWeek.cs
@@ -0,0 +1,15 @@
+﻿namespace _08.Day_Of_Week
+{
+	using System;
+
+	public static class DayOfWeek
+	{
+		public static void Main()
+		{
+			var day = int.Parse(Console.ReadLine());
+
+			day = day != 7 ? day : 0;
+			Console.WriteLine((System.DayOfWeek)day);
+		}
+	}
+}
