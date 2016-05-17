af6059aee06698034969373cd78dd3a88e595c93
Housey <housebreaker1@gmail.com>
2016-05-17 14:59:54 +0300

    add problem 2

---
+++ 01. Data Types and Methods/02. Circle Area (12 Digits Precision)/CircleArea.cs
@@ -0,0 +1,16 @@
+﻿namespace _02.Circle_Area__12_Digits_Precision_
+{
+	using System;
+
+	public static class CircleArea
+	{
+		public static void Main()
+		{
+			var radius = double.Parse(Console.ReadLine());
+
+			var area = Math.PI * radius * radius;
+
+			Console.WriteLine(area.ToString("F12"));
+		}
+	}
+}
