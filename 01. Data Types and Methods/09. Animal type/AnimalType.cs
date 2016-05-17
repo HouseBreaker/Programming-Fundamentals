29db1b99a59e412d302aad7a08cad2dc80a6fede
Housey <housebreaker1@gmail.com>
2016-05-17 15:53:24 +0300

    add problem 9

---
+++ 01. Data Types and Methods/09. Animal type/AnimalType.cs
@@ -0,0 +1,30 @@
+﻿namespace _09.Animal_type
+{
+	using System;
+
+	public static class AnimalType
+	{
+		public static void Main()
+		{
+			var animal = Console.ReadLine();
+
+			string type;
+			switch (animal)
+			{
+				case "dog":
+					type = "mammal";
+					break;
+				case "crocodile":
+				case "tortoise":
+				case "snake":
+					type = "reptile";
+					break;
+				default:
+					type = "unknown";
+					break;
+			}
+
+			Console.WriteLine(type);
+		}
+	}
+}
