d7d42396b10a48f423d141809c8bbdd816e61189
Housey <housebreaker1@gmail.com>
2016-05-17 16:08:59 +0300

    add problem 10

---
+++ 01. Data Types and Methods/10. Filled Square/FilledSquare.cs
@@ -0,0 +1,23 @@
+﻿namespace _10.Filled_Square
+{
+	using System;
+	using System.Linq;
+
+	public static class FilledSquare
+	{
+		public static void Main()
+		{
+			var size = int.Parse(Console.ReadLine());
+
+			Console.WriteLine(new string('-', size * 2));
+
+			for (int i = 0; i < size / 2; i++)
+			{
+				var slashes = string.Concat(Enumerable.Repeat(@"\/", size - 1));
+				Console.WriteLine("-" + slashes + "-");
+			}
+
+			Console.WriteLine(new string('-', size * 2));
+		}
+	}
+}
