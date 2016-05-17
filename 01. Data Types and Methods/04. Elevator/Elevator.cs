c1970eddc02bdb84b3a67ee979cf4759ecb82c6a
Housey <housebreaker1@gmail.com>
2016-05-17 15:11:14 +0300

    add problem 4

---
+++ 01. Data Types and Methods/04. Elevator/Elevator.cs
@@ -0,0 +1,16 @@
+﻿namespace _04.Elevator
+{
+	using System;
+
+	public static class Elevator
+	{
+		public static void Main()
+		{
+			var peopleCount = int.Parse(Console.ReadLine());
+			var capacity = int.Parse(Console.ReadLine());
+
+			var coursesNeeded = Math.Ceiling((double)peopleCount / capacity);
+			Console.WriteLine(coursesNeeded);
+		}
+	}
+}
