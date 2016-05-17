ad000140b8cc862ac77fd2b34dd7b22093978e8d
Housey <housebreaker1@gmail.com>
2016-05-17 15:00:05 +0300

    add problem 3

---
+++ 01. Data Types and Methods/03. Exact Sum of Real Numbers/ExactSum.cs
@@ -0,0 +1,22 @@
+﻿namespace _03.Exact_Sum_of_Real_Numbers
+{
+	using System;
+	using System.Linq;
+
+	public static class ExactSum
+	{
+		public static void Main()
+		{
+			var count = int.Parse(Console.ReadLine());
+			
+			var numbers = new decimal[count];
+			for (int i = 0; i < count; i++)
+			{
+				numbers[i] = decimal.Parse(Console.ReadLine());
+			}
+
+			var sum = numbers.Sum();
+			Console.WriteLine(sum);
+		}
+	}
+}
