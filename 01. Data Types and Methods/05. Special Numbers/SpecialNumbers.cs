04b6e01f2fa6d6e74c543c2437d7173944f82ceb
Housey <housebreaker1@gmail.com>
2016-05-17 15:11:29 +0300

    add problem 5

---
+++ 01. Data Types and Methods/05. Special Numbers/SpecialNumbers.cs
@@ -0,0 +1,26 @@
+﻿namespace _05.Special_Numbers
+{
+	using System;
+	using System.Linq;
+
+	public static class SpecialNumbers
+	{
+		public static void Main()
+		{
+			var count = int.Parse(Console.ReadLine());
+
+			var range = Enumerable.Range(1, count);
+
+			foreach (var num in range)
+			{
+				var numbers = num.ToString().ToArray().Select(a => int.Parse(a.ToString())).ToArray();
+
+				var sum = numbers.Sum();
+
+				var special = sum == 5 || sum == 7 || sum == 11;
+
+				Console.WriteLine($"{num} -> {special}");
+			}
+		}
+	}
+}
